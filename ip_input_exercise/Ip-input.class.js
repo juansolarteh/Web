2026b6a2eaca3bc9c79858b80f77f0123592d5b7
JuanJpsh <113944776+JuanJpsh@users.noreply.github.com>
2023-03-19 00:07:51 -0500

    input id done

--- ip_input_exercise/Ip-input.class.js
+++ ip_input_exercise/Ip-input.class.js
@@ -1,22 +1,28 @@
 function IpInput(input) {
-  this.ipLength = 0;
-  this.byteLength = 0;
-  this.byteSection = 1;
-  this.nextPointDelete = false;
   this.valid = false;
-  this.value = ""
+  this.value = "";
+  
+  let ipLength = 0;
+  let byteLength = 0;
+  let byteSection = 1;
+  let nextPointDelete = false;
 
-  input.placeholder = "_ _ _ . _ _ _ . _ _ _ . _ _ _"
+  input.placeholder = "_ _ _ . _ _ _ . _ _ _ . _ _ _";
 
   input.addEventListener("input", ($event) => {
     const value = $event.target.value;
 
-    if (value.length < this.ipLength) {
-      if (this.nextPointDelete) handlePointDelete(value);
+    if (value.length < ipLength) {
+      if (nextPointDelete) handlePointDelete(value);
       handleCharacterDelete(input.value);
-      this.value = input.value
-      if (this.byteLength == 0) this.valid = false;
+      this.value = input.value;
+      if (byteLength == 0) this.valid = false;
       return;
+    }
+
+    if (input.selectionStart - 1 != ipLength) {
+      input.value = removeCharacter(value, input.selectionStart - 1)
+      return
     }
 
     if (!isValidCharacter(value[value.length - 1])) {
@@ -24,69 +30,74 @@ function IpInput(input) {
       return;
     }
 
-    if (this.byteSection == 4 && this.byteLength == 3) {
+    if (byteSection == 4 && byteLength == 3) {
       input.value = removeLastCharacter(value);
       return;
     }
 
-    this.value = input.value
+    this.value = input.value;
     if (value[value.length - 1] == ".") return handlePointWrite();
     handleCharacterWrite();
 
-    if (this.byteLength == 0 && this.byteSection != 1) writePoint();
+    if (byteLength == 0 && byteSection != 1) writePoint();
 
-    if (this.byteSection == 4 && this.byteLength > 0) this.valid = true;
+    if (byteSection == 4 && byteLength > 0) this.valid = true;
   });
 
   const isValidCharacter = (character) => {
     return Number(character) ||
-      (character == "0" && this.byteLength != 0) ||
-      (character == "." && this.byteLength != 0 && this.byteSection != 4)
+      (character == "0" && byteLength != 0) ||
+      (character == "." && byteLength != 0 && byteSection != 4)
       ? true
       : false;
   };
 
   const removeLastCharacter = (value) => {
     return value.slice(0, value.length - 1);
-  }
+  };
+
+  const removeCharacter = (value, position) => {
+    const aux = value.slice(0, position);
+    return aux + value.slice(position + 1);
+  };
 
   const handleCharacterDelete = (value) => {
-    this.ipLength -= 1;
-    this.byteLength -= 1;
-    if (this.byteLength < 1) {
-      this.byteLength = value.split(".")[this.byteSection - 1].length;
+    ipLength -= 1;
+    byteLength -= 1;
+    if (byteLength < 1) {
+      byteLength = value.split(".")[byteSection - 1].length;
     }
-    if (value[value.length - 1] == ".") this.nextPointDelete = true;
-  }
+    if (value[value.length - 1] == ".") nextPointDelete = true;
+  };
 
   const handlePointDelete = (value) => {
     input.value = removeLastCharacter(value);
-    this.ipLength -= 1;
-    this.byteSection -= 1;
-    this.nextPointDelete = false;
-  }
+    ipLength -= 1;
+    byteSection -= 1;
+    nextPointDelete = false;
+  };
 
   const handleCharacterWrite = () => {
-    this.ipLength += 1;
-    this.byteLength += 1;
-    if (this.byteLength == 3 && this.byteSection < 4) {
-      this.byteSection += 1;
-      this.byteLength = 0;
+    ipLength += 1;
+    byteLength += 1;
+    if (byteLength == 3 && byteSection < 4) {
+      byteSection += 1;
+      byteLength = 0;
     }
-    this.nextPointDelete = false;
-  }
+    nextPointDelete = false;
+  };
 
   const handlePointWrite = () => {
-    this.byteSection += 1;
-    this.ipLength += 1;
-    this.byteLength = 0;
-    this.nextPointDelete = true;
-  }
+    byteSection += 1;
+    ipLength += 1;
+    byteLength = 0;
+    nextPointDelete = true;
+  };
 
   const writePoint = () => {
     input.value += ".";
-    this.value += "."
-    this.ipLength += 1;
-    this.nextPointDelete = true;
-  }
+    this.value += ".";
+    ipLength += 1;
+    nextPointDelete = true;
+  };
 }
